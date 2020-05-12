e66d6cc359e71f42244b765f03d8b68487754936
Éder Pedroso Gadiolli <eder.pedroso@stoom.com.br>
2020-05-12 14:40:23 -0300

    acerto de chao

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles.scss';
+import './Styles/global.scss';
 import { Player } from './Core/Entities/Player';
 import ChangePosition from './Core/Systems/ChangePosition';
 import GridContainer from './Containers/GridContainer/GridContainer';
