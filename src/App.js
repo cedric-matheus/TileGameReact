5fee3ff08b84ce296e78e075263dd3a74a4770e8
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-05-12 00:38:52 +0000

    Adicionando arquivos do projeto antigo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles.scss';
+import { Player } from './Core/Entities/Player';
+import ChangePosition from './Core/Systems/ChangePosition';
+import GridContainer from './Containers/GridContainer/GridContainer';
 
-function App() {
+export default function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <GridContainer />
+
+      <div onClick={() => (Player = ChangePosition(Player, 5, 0))}>
+        Posição X: {Player.position.row}
+      </div>
+    </React.Fragment>
   );
 }
-
-export default App;
