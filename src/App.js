56b6b50ce47d7ba28b350db039d79543dcec29ee
Éder Pedroso Gadiolli <eder.pedroso@stoom.com.br>
2020-05-13 18:42:19 -0300

    acerto da borda

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './Styles/global.scss';
 import { Player } from './Core/Entities/Player';
 import ChangePosition from './Core/Systems/ChangePosition';
 import GridContainer from './Containers/GridContainer/GridContainer';
+import BorderComponent from './Components/BorderComponent/BorderComponent';
 
 export default function App() {
   return (
     <React.Fragment>
-      <div className='border'></div>
       <GridContainer />
-
+      <BorderComponent />
       <div onClick={() => (Player = ChangePosition(Player, 5, 0))}>
         Posição X: {Player.position.row}
       </div>
---
+++ src/Components/BorderComponent/BorderComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../BorderComponent/BorderComponent.scss';
+
+/** 
+  * @Name: Border Component
+  * @Desc: Uma borda de sombra que fica em toda a volta do jogo
+  * @props: 
+  *     
+
+  */
+
+const BorderComponent = () => {
+  return (
+    <React.Fragment>
+      <div className='BorderComponent'></div>
+    </React.Fragment>
+  );
+};
+
+export default BorderComponent;
