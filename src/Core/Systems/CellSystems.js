ad17f63c98111825778cdbc4c88952149de0aa4a
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-05-14 03:59:03 +0000

    Sistema para criar celula (createCell) criado

--- src/Core/Systems/CellSystems.js
+++ src/Core/Systems/CellSystems.js
@@ -1,13 +1,27 @@
 // CORE SYSTEMS
 import { getRandomIntegerNumber, getRandomBooleanValue } from './RandomSystem';
 
+// CORE COMPONENTS
+import CellComponent from '../Components/Cell';
+
+// JSDOC TYPEDEFS
+import '../Components/Cell/typedef';
+
 /**
- * Esse sistema é responsável por gerar uma célula (CellComponent) randômica.
+ * Esse sistema é responsável por criar uma célula (CellComponent) randômica.
  *
  * @returns {CellComponent} Célula gerada randômicamente.
  */
-export function generateCell() {
+export function createCell() {
+  const randomCell = { ...CellComponent };
+
   const type = getRandomIntegerNumber(3);
   const life = getRandomIntegerNumber(5);
   const isInverted = getRandomBooleanValue();
+
+  randomCell.type = type;
+  randomCell.life = life;
+  randomCell.isInverted = isInverted;
+
+  return randomCell;
 }
