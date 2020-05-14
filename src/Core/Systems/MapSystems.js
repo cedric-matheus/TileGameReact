047ca5ff8a3247d1cc41bb9b6dfdc1031dac10aa
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-05-14 03:57:56 +0000

    Sistema (ecs) para criar mapa (createMap) criado

--- src/Core/Systems/MapSystems.js
+++ src/Core/Systems/MapSystems.js
@@ -1,9 +1,39 @@
+// CORE SYSTEMS
+import { createCell } from './CellSystems';
+
+// CORE COMPONENTS
+import MapComponent from '../Components/Map';
+
+// JSDOC TYPEDEFS
+import '../Components/Map/typedef';
+
 /**
- * Esse sistema é responsável por gerar um mapa randomico com a altura (height)
+ * Esse sistema é responsável por criar um mapa randômico com a altura (height)
  * e largura (width) informados.
  *
- * @param {object} entity Entidade que o sistema vai trabalhar
  * @param {number} width Largura do mapa
  * @param {number} height Altura do mapa
+ *
+ * @returns {MapComponent} Mapa gerado randômicamente
  */
-export function generateMap(entity, width, height) {}
+export function createMap(width, height) {
+  const randomMap = { ...MapComponent };
+
+  // gerando linhas do mapa
+  for (let row = 0; row < height; row++) {
+    const mapRow = [];
+
+    // gerando colunas da linha
+    for (let column = 0; column < width; column++) {
+      const rowColumn = createCell();
+
+      // adicionando coluna a linha
+      mapRow.push(rowColumn);
+    }
+
+    // adicionando linha ao mapa (matriz)
+    randomMap.mapMatrix.push(mapRow);
+  }
+
+  return randomMap;
+}
