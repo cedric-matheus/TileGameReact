1ef590e62fd1c9871cfe0b099bb774dbb5fdebd8
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-05-14 15:44:10 +0000

    Refatorando componente ecs map

--- src/Core/Components/Map/index.js
+++ src/Core/Components/Map/index.js
@@ -1,16 +1,10 @@
-// CORE COMPONENTS
-import CellComponent from '../Cell';
-
-// JSDOC TYPEDEFS
-import '../Cell/typedef';
-
 /**
  * Componente Mapa (MapComponent)
  *
  * Esse componente é responsável por representar um mapa (matriz)
  *
- * @property {CellComponent[]} mapMatrix=`[]` - Matriz representativa do mapa
+ * @property {Array} mapMatrix=`[]` - Matriz representativa do mapa
  */
-const MapComponent = { mapMatrix: [[{ ...CellComponent }]] };
+const MapComponent = { mapMatrix: [] };
 
 export default MapComponent;
--- src/Core/Components/Map/typedef.js
+++ src/Core/Components/Map/typedef.js
@@ -1,11 +1,9 @@
-/* eslint-disable jsdoc/no-undefined-types */
-import '../Cell/typedef';
-
 /**
  * Componente Mapa (MapComponent)
  *
  * Esse componente é responsável por representar um mapa (matriz)
  *
  * @typedef {object} MapComponent
- * @property {CellComponent[]} mapMatrix=`[]` - Matriz representativa do mapa
+ *
+ * @property {Array} mapMatrix=`[]` - Matriz representativa do mapa
  */
